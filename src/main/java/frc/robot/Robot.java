694ead3945c84c1f0d302d723f169402cc276ed4
Finn <148401810+FinnSpartronics@users.noreply.github.com>
2024-01-08 19:45:49 -0800

    Added some lighting that may or may not work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -19,6 +21,13 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  public AddressableLED led;
+  public AddressableLEDBuffer ledBuffer;
+  public float ledLength = 60f;
+
+  public int mode = 0;
+  public int frame = 0;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -28,6 +37,19 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    led = new AddressableLED(0);
+
+    ledBuffer = new AddressableLEDBuffer((int) ledLength);
+    for (var i = 0; i < ledBuffer.getLength(); i++) {
+      float r = (255f-(255f/ledLength)*i);
+      float g = (255f/ledLength)*i;
+      ledBuffer.setRGB(i, (int) r, (int) g, 50);
+    }
+    led.setLength(ledBuffer.getLength());
+
+    led.setData(ledBuffer);
+    led.start();
   }
 
   /**
@@ -44,6 +66,23 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    frame++;
+    if (mode == 1) {
+      for (var i = 0; i < ledBuffer.getLength(); i++) {
+        float c = (float) (Math.sin(frame)*255f);
+        ledBuffer.setRGB(i, (int) (int) Math.max(c-20,0), (int) Math.max(c-20,0), (int) c);
+      }
+      led.setData(ledBuffer);
+    } else if (mode == 2) {
+      if (frame >= 60) frame -= 60;
+      for (var i = 0; i < ledBuffer.getLength(); i++) {
+        float c = 255f/(i-frame+1);
+        if (i-frame > 5)
+          ledBuffer.setRGB(i, (int) (int) c, (int) c, (int) c);
+        else
+          ledBuffer.setRGB(i, 0, 0, 0);
+      }
+    }
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
